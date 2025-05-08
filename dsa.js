c3cce873c3f520b772c908f228c347160b1568b5
Ashish Rai <ashishrai8880@gmail.com>
2025-05-08 12:38:41 +0530

    Update dsa.js

--- dsa.js
+++ dsa.js
@@ -113,3 +113,120 @@ const pipe = (...fns)=>{
 
 console.log(pipe(getName , uppercase , reverse)(data))
 
+
+// 4. ======================================================= Quick Sort ===========================================================
+// 4. ======================================================= Quick Sort ===========================================================
+
+function partition(arr, low, high) {
+    let pivot = arr[high];
+    let i = low - 1;
+
+    for (let j = low; j <= high - 1; j++) {
+        // If current element is smaller than the pivot
+        if (arr[j] < pivot) {
+            // Increment index of smaller element
+            i++;
+            // Swap elements
+            [arr[i], arr[j]] = [arr[j], arr[i]]; 
+        }
+    }
+    // Swap pivot to its correct position
+    [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]]; 
+    return i + 1; // Return the partition index
+}
+
+function quickSort(arr, low, high) {
+    if (low >= high) return;
+    let pi = partition(arr, low, high);
+
+    quickSort(arr, low, pi - 1);
+    quickSort(arr, pi + 1, high);
+}
+
+let arr = [10, 80, 30, 90, 40];
+console.log("Original array: " + arr);
+
+quickSort(arr, 0, arr.length - 1);
+console.log("Sorted array: " + arr);
+
+
+
+// 4. ======================================================= Quick Sort ===========================================================
+// 4. ======================================================= Quick Sort ===========================================================
+
+
+
+function merge(arr=[] , left , mid , right){
+    
+    let l1 = mid - left + 1;
+    let l2 = right - mid;
+    
+    let arr1 = new Array(l1);
+    let arr2 = new Array(l2);
+    
+     // Assign values in subarrays
+    for (let i = 0; i < l1; ++i) {
+        arr1[i] = arr[left + i];
+    }
+    for (let i = 0; i < l2; ++i) {
+        arr2[i] = arr[mid + 1 + i];
+    }
+    
+    
+    // To travesrse and modify main array
+    let i = 0,
+        j = 0,
+        k = left;
+    
+     while (i < l1 && j < l2) {
+        if (arr1[i] < arr2[j]) {
+            arr[k] = arr1[i];
+            ++i;
+        } else {
+            arr[k] = arr2[j];
+            j++;
+        }
+         k++;
+    }
+    
+    // Update the remaining elements
+    while (i < l1) {
+        arr[k] = arr1[i];
+        i++;
+        k++;
+    }
+    while (j < l2) {
+        arr[k] = arr2[j];
+        j++;
+        k++;
+    }
+    
+    
+}
+
+function mergeSort(arr=[] , left , right){
+    
+    if(left >= right){
+        return ;
+    }
+    
+    let mid = left + parseInt((right-left)/2);
+    
+    mergeSort(arr , left , mid) ;
+    mergeSort(arr , mid+1 , right);
+    
+    merge(arr  , left , mid , right);
+}
+
+// Input array
+const arr =  [ 38, 27, 43, 10]
+
+// Display input array
+console.log("Original array: " + arr);
+
+// Apply merge sort function
+mergeSort(arr, 0, arr.length - 1);
+
+// Display output
+console.log("After sorting: " + arr);
+
