910398c9bb301d9260f6ff990f99fb4f8e242794
Ashish Rai <ashishrai8880@gmail.com>
2025-06-29 20:34:31 +0530

    Update Basic.js

--- DSA/Graph/Basic.js
+++ DSA/Graph/Basic.js
@@ -1,9 +1,9 @@
 /*
-1 --- 3
-/ | \
-0 | 5 -- 6
-\ | /
-2 ---- 4
+ 1 --- 3
+/      |  \
+0      |     5 -- 6
+\      |   /
+ 2 ---- 4
 */
 
 //Helper Queue
@@ -45,10 +45,7 @@ class Edge{
 class Graph{
     
     constructor(){
-        this.graph = [] ;
-        for(let i=0 ; i< 4 ; i++){
-            this.graph[i] = [];
-        }
+        this.graph = Array.from({length : 7 } , ()=>[]) ;
     }
     
     createGraph(){
@@ -107,8 +104,39 @@ class Graph{
                 })
             }
              q.pop();
-        }
+        }  
+    }
+    dfsTraversal(root , isVisited=[]){
+        console.log(root) ;
+        isVisited[root] = true 
         
+        const vertex = this.graph[root] || [];
+        
+        //calling for child first of current vertex
+        for(let i=0 ; i<vertex.length ; i++){
+            const child = vertex[i].dest ;
+            if(isVisited[child] ==false){
+                this.dfsTraversal(child , isVisited);
+            }
+        }
+    }
+
+    
+    printAllPaths(src , target , path="" , isVisited=[]){
+        if(src == target){
+            console.log(path);
+            return ; 
+        }
+    
+        const currVertex = this.graph[src] || [];
+    
+        currVertex.forEach((edge)=>{
+            if(isVisited[edge.dest] == false){
+                isVisited[edge.dest] = true ;
+                this.printAllPaths(edge.dest , target , path+`${edge.dest}` , isVisited) ;
+                isVisited[edge.dest] = false ;
+            }
+        })
     }
 }
 
@@ -116,8 +144,8 @@ const graph = new Graph();
 graph.createGraph()
 graph.printNeighbours(2)
 
-// bfs traversal
-graph.bfsTraversal(0) ;
+// bfs traversal . Time complexity : O (V + E) = O (N) . V is number of vertex and E is number of edges . This TC will be same for second bfsTraversalInBrokenGraph also .
+graph.bfsTraversal(0) ; 
 
 // for broken graph 
 const isVisited = Array.from({ length: 4 }, () => false);
@@ -126,5 +154,20 @@ for(let i =0 ; i< isVisited.length ; i++){
         graph.bfsTraversalInBrokenGraph(i , isVisited)
     }
 }
+
+// dfs Traversal
+graph.dfsTraversal(0 , isVisited) ;
+
+// dfs Traversal for broken graph /seperated graph
+for(let i =0 ; i<isVisited.length ; i++){
+    if(isVisited[i] == false){
+        graph.dfsTraversal(i , isVisited) ;
+    }
+}
+
+
+// print all path
+graph.printAllPaths(0 , 5 , "0" , isVisited)
+    
 
 
