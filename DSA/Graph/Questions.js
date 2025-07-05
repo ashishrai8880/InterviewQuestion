6b4363fc1362e387a6cdcfc1e5aedf2ac1d3df48
Ashish Rai <ashishrai8880@gmail.com>
2025-07-05 22:46:26 +0530

    Course Schedule Graph Question

--- DSA/Graph/Questions.js
+++ DSA/Graph/Questions.js
@@ -155,3 +155,99 @@ var numIslands = function(grid) {
 
 
 };
+
+
+// ===================================3. Course Schedule ============================================================
+// Leetcode Link : https://leetcode.com/problems/course-schedule/submissions/1687514818/
+// There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+// For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+// Return true if you can finish all courses. Otherwise, return false.
+
+// Example 1:
+
+// Input: numCourses = 2, prerequisites = [[1,0]]
+// Output: true
+// Explanation: There are a total of 2 courses to take. 
+// To take course 1 you should have finished course 0. So it is possible.
+// Example 2:
+
+// Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+// Output: false
+// Explanation: There are a total of 2 courses to take. 
+// To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
+  const graph = new Graph(numCourses);
+  graph.createGraph(numCourses, prerequisites);
+  return !graph.isCyclic()  
+};
+
+class Edge{
+   
+    constructor(src , dest){
+        this.src = src ;
+        this.dest = dest ;
+    }
+}
+
+class Graph{
+    
+    constructor(size){
+        this.graph = Array.from({length : size} , ()=>[]);
+    }
+    
+    createGraph(numCourse , preRequisite=[]){
+        
+        preRequisite.forEach((ele , index)=>{
+            const [ after , before ] = ele ;
+            this.graph[before].push(new Edge(before , after));
+        })
+    }
+    
+    isCyclicUtil(curr , isVisited=[] , callStack=[]){
+        
+        isVisited[curr] = true ;
+        callStack[curr] = true ;
+        
+        const vertex = this.graph[curr] ;
+        
+        for(let i = 0 ; i<vertex.length ; i++){
+            const dest = vertex[i].dest ;
+            
+            if(callStack[dest] == true){
+                return true ; //contains cycle
+            }
+            else if(isVisited[dest] == false){
+                const r = this.isCyclicUtil(dest , isVisited , callStack);
+                if(r== true){
+                    return true ;
+                }
+            }
+        }
+        callStack[curr] = false ;
+        return false ;
+    }
+    
+    isCyclic(){
+        
+        let isVisited = Array.from({length : this.graph.length} , ()=>false);
+        let callStack = [];
+        
+        for(let i = 0 ; i<this.graph.length ; i++){
+            if(isVisited[i] == false){
+                const r= this.isCyclicUtil(i , isVisited , callStack);
+                if(r == true){
+                    return true ;
+                }
+            }
+        }
+        return false ;
+    }
+}
+
