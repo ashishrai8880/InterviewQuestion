c547c1f5823172ab86106ec38140a45c3f59493e
Ashish Rai <ashishrai8880@gmail.com>
2025-08-11 11:18:51 +0530

    Sum Of Array

--- DSA/DP/BasicQuestions.js
+++ DSA/DP/BasicQuestions.js
@@ -10,3 +10,19 @@ class Solution {
         return n * this.factorial(n-1);
     }
 }
+
+// ========================================================================== 2. Sum of array ==========================================================================================================
+/* 
+  Base condition would be if n ==0 then return that number . Express like arr[n] + arr[n-1] ;
+*/
+class Solution {
+    arraySum(arr) {
+        const sumUtil =(n)=>{
+            if(n==0){
+                return arr[0];
+            }
+            return arr[n] + sumUtil(n-1)
+        }
+        return sumUtil(arr.length-1);
+    }
+}
