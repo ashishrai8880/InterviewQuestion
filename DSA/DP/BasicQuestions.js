d4bc0a5b2bff031c5e6f0fee44c5d7eae5095563
Ashish Rai <ashishrai8880@gmail.com>
2025-08-12 10:40:53 +0530

    Update BasicQuestions.js

--- DSA/DP/BasicQuestions.js
+++ DSA/DP/BasicQuestions.js
@@ -26,3 +26,35 @@ class Solution {
         return sumUtil(arr.length-1);
     }
 }
+
+// =============================================================3. Power of a Number ==============================================================================
+// Brute force Recursion . It can be little bit optimized by just by modulos by Infinity for a large number .
+class Solution {
+   power(n , pow)   {
+        if(pow == 0){
+            return 1 ;
+        }
+        return n * power(n , pow-1);
+    }   
+}
+
+// Optimized way , 2^8 will be 2^4 * 2 ^ 4 =>  16*16 = 256 which is 2^8 . So instead of callig stack for every number , just reduced it half , and then call it , multiply 
+// answer with answer if power is even , if power is odd then multiply answer with answer with single n also . 
+class Solution {
+   power(n , pow)   {
+      if(pow == 0){
+           return 1 ;
+       }
+       
+       if(pow % 2 == 0){
+           const ans = power(n , pow/2);
+           return ans * ans ;
+       }
+       else{
+           const ans = power(n , (pow-1)/2);
+           return ans * ans * n ;
+       }
+    
+	       return x * power( x , n-1 );
+   }
+}
