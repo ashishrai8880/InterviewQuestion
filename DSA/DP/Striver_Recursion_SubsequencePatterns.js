1711cf06543e77bae27594d3773cc20bfef9603e
Ashish Rai <ashishrai8880@gmail.com>
2025-08-24 10:45:05 +0530

    Update Striver_Recursion_SubsequencePatterns.js

--- DSA/DP/Striver_Recursion_SubsequencePatterns.js
+++ DSA/DP/Striver_Recursion_SubsequencePatterns.js
@@ -78,6 +78,42 @@ var generateParenthesis = function(n) {
 };
 
 
+// ========================================================================3. Find All Subsets ==============================================================================
+/**
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ */
+var subsets = function(nums) {
+    
+    const util = (i , subset)=>{
+        if(i == nums.length){
+            res.push([...subset]);
+            return ;
+        }
+
+        subset.push(nums[i]);
+        util(i+1 , subset);
+        subset.pop();
+        util(i+1 , subset);
+    }
+
+    let res = [];
+    let subset = [];
+    util(0 , subset );
+    return res ;
+
+};
+
+
+
+
+
+
 
 
 
