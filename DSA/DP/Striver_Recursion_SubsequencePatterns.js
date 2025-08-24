7c8baa2dc1ee3ee25eb4053839d3eb45874fce21
Ashish Rai <ashishrai8880@gmail.com>
2025-08-24 17:24:01 +0530

    Subset Sum 1 Leetcode

--- DSA/DP/Striver_Recursion_SubsequencePatterns.js
+++ DSA/DP/Striver_Recursion_SubsequencePatterns.js
@@ -193,6 +193,47 @@ var combinationSum = function(arr, target) {
 };
 
 
+// =======================================================================5. Subset Sum 1 =================================================================================
+/**
+GFG : https://www.geeksforgeeks.org/problems/subset-sums2234/1&selectedLang=python3
+Given a array arr of integers, return the sums of all subsets in the list.  Return the sums in any order.
+
+Examples:
+Input: arr[] = [2, 3]
+Output: [0, 2, 3, 5]
+Explanation: When no elements are taken then Sum = 0. When only 2 is taken then Sum = 2. When only 3 is taken then Sum = 3. When elements 2 and 3 are
+taken then Sum = 2+3 = 5.
+ */
+class Solution {
+    subsetSums(arr) {
+        
+        const util = (i , sum)=>{
+            
+            if(i == arr.length){
+                res.push(sum);
+                return ;
+            }
+            
+            util(i+1 , sum + arr[i]);
+            util(i+1 , sum);
+            
+        }
+        
+        let res = [];
+        util(0 , 0);
+        return res ;
+        
+    }
+}
+
+
+
+
+
+
+
+
+
 
 
 
