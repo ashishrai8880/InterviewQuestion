8be8e865e2c0aad4775f7d29bc06171cb6489459
Ashish Rai <ashishrai8880@gmail.com>
2025-08-26 13:20:00 +0530

    Letter Combination of Phone Number Cool

--- DSA/DP/Striver_Recursion_SubsequencePatterns.js
+++ DSA/DP/Striver_Recursion_SubsequencePatterns.js
@@ -393,8 +393,54 @@ var subsetsWithDup = function(nums) {
 };
 
 
+// =======================================================================9. Letter Combination of Phone Number ====================================================================================
+/**
+Leetcode : https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+Time Complexity : O(4^n)
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 
+Example 1:
 
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+Example 2:
+
+Input: digits = ""
+Output: []
+Example 3:
+
+Input: digits = "2"
+Output: ["a","b","c"]
+ */
+var letterCombinations = function(digits) {
+    
+    const util = (index , combination)=>{
+        if(index >= len){
+            res.push(combination);
+            return ;
+        }
+
+        const ch = digits[index];
+        const value = mapping[ch];
+
+        for(let i=0 ; i<value.length ; i++){
+            util(index+1 ,  combination + value[i]) ;
+        }
+
+    }
+
+    if(digits.length == 0){
+        return [];
+    }
+
+    const mapping = ["","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"] ;
+    const len = digits.length ;
+    let res = [];
+    util(0 , "");
+    return res ;
+
+};
 
 
 
