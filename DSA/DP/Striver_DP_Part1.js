8fcea9546e4022ebc9cd60584c6cd6ae034ed149
Ashish Rai <ashishrai8880@gmail.com>
2025-09-01 17:55:03 +0530

    Ninja Traning

--- DSA/DP/Striver_DP_Part1.js
+++ DSA/DP/Striver_DP_Part1.js
@@ -268,6 +268,71 @@ var rob = function(nums) {
 };
 
 
+// ====================================================================== 5. Ninja Training ================================================================================
+/*
+Geek is going for a training program for n days. He can perform any of these activities: Running, Fighting, and Learning Practice. Each activity has some point on each day. As Geek wants to improve all his skills, he can't do the same
+activity on two consecutive days. Given a 2D array arr[][] of size n where arr[i][0], arr[i][1], and arr[i][2] represent the merit points for Running, Fighting, and Learning on the i-th day, determine the maximum total merit points Geek can achieve .
+
+Example:
+Input: arr[]= [[1, 2, 5], [3, 1, 1], [3, 3, 3]]
+Output: 11
+Explanation: Geek will learn a new move and earn 5 point then on second day he will do running and earn 3 point and on third day he will do fighting and earn 3 points so, maximum merit point will be 11.
+
+Input: arr[]= [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
+Output: 6
+Explanation: Geek can perform any activity each day while adhering to the constraints, in order to maximize his total merit points as 6.
+*/
+
+class Solution {
+    // Function to find the maximum points among all the possible ones.
+    maximumPoints(arr) {
+        // your code here
+        const len = arr.length ;
+        if(len == 1){
+            return Math.max(arr[0][0] , Math.max(arr[0][1] , arr[0][2 ])  )
+        }
+        
+        const util = (row , prev )=>{
+            
+            if(dp[row][prev] !== -1) return dp[row][prev] ;
+            
+            if(row == arr.length-1 ){
+                let max = -Infinity ;
+                arr[len-1].forEach((e , i)=>{
+                    if(i != prev){
+                        max = Math.max(e , max);
+                    }
+                })
+                
+                return max ;
+            }
+            
+            
+            
+            
+            let max = -Infinity ;
+            
+            for(let i=0 ; i< 3 ;i++){
+                if(i != prev){
+                    let res = arr[row][i] + util(row+1 , i);
+                    
+                    max = Math.max(max , res);
+                }
+            }
+            dp[row][prev] = max ;
+            return max ;
+            
+        }
+        
+        let dp = Array.from({length : len+1} , ()=>{
+            return Array.from({length : 4},()=>-1)
+        })
+        return util(0 , 3);
+        
+    }
+}
+
+
 
 
 
