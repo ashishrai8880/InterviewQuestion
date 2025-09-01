1068604ad81d55a591fff890c3ff8f9c53eb6e8b
Ashish Rai <ashishrai8880@gmail.com>
2025-09-01 22:28:38 +0530

    Create Codechef.js

---
+++ DSA/Contest/Codechef.js
@@ -0,0 +1,38 @@
+// ======================================================================1. Valid Palindrome ============================================================================
+/**
+Leetcode : https://leetcode.com/problems/valid-palindrome-ii/description/
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+Example 1:
+Input: s = "aba"
+Output: true
+
+Example 2:
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+ */
+var validPalindrome = function(s) {
+    
+    const isPalindrome = (left , right)=>{
+        while(left < right){
+            if(s[left] != s[right]) return false ;
+            left+=1 ;
+            right-=1 ;
+        }
+        return true ;
+    }
+
+    let left = 0 ;
+    let right = s.length-1 ;
+
+    while(left < right){
+        if(s[left] !== s[right]){
+            return isPalindrome(left+1,right) || isPalindrome(left,right-1)
+        }
+        left+=1 ;
+        right-=1 ;
+    }
+
+    return true ;
+
+};
