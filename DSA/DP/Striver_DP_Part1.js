33fdbe8498928819ece24d47da48046c985bbcae
Ashish Rai <ashishrai8880@gmail.com>
2025-09-02 15:15:13 +0530

    Count Unique Path 2

--- DSA/DP/Striver_DP_Part1.js
+++ DSA/DP/Striver_DP_Part1.js
@@ -384,5 +384,42 @@ var uniquePaths = function(m, n) {
 };
 
 
+// ======================================================================== 7. Count Unique Path 2 =============================================================================
+/**
+You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at 
+any point in time.
+An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+The testcases are generated so that the answer will be less than or equal to 2 * 109.
+ */
+var uniquePathsWithObstacles = function(grid) {
+    
+    const util = (i , j)=>{
+
+        if(i==row-1 && j==col-1) {
+            if(grid[i][j] == 0) return 1 ;
+            return 0 ;
+        };
+
+        if(i >= row || j>= col || i<0 || j<0 || grid[i][j] ==1) return 0 ;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        const right = util(i,j+1);
+        const down = util(i+1 , j);
+
+        return dp[i][j] = right + down ;
+
+    }
+
+    const row = grid.length ;
+    const col = grid[0].length ;
+    let dp = Array.from({length : row+1},()=>{
+        return Array.from({length : col+1} , ()=>-1)
+    })
+
+    return util(0 , 0);
+
+};
 
 
