24e300b3f6fd357db99b7319f03c0e66034a82c6
Ashish Rai <ashishrai8880@gmail.com>
2025-09-05 13:21:45 +0530

    Update Basic.js

--- DSA/Binary Tree/Basic.js
+++ DSA/Binary Tree/Basic.js
@@ -323,6 +323,35 @@ var isBalanced = function(root) {
 };
 
 
+// =========================================================== Diameter of Binary Tree ============================================================================
+/**
+Leetcode : https://leetcode.com/problems/diameter-of-binary-tree/
+Given the root of a binary tree, return the length of the diameter of the tree.
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+The length of a path between two nodes is represented by the number of edges between them.
+
+Solution : If we can calculate sum of left height and right height , then it can also give diameter . I subtracted 2 from diameter because , for both side height it is taking root node of any subtree 2 
+times . 
+ */
+var diameterOfBinaryTree = function(root) {
+    
+    const util = (ptr)=>{
+        if(ptr == null) return 0 ;
+
+        const left = 1 + util(ptr.left);
+        const right = 1 + util(ptr.right);
+
+        const diameter = left + right ;
+        ans = Math.max(ans , diameter);
+
+        return Math.max(left , right);
+    }
+
+    ans = 0 ;
+    util(root);
+    return ans == 0 ? 0 : ans-2 ;
+
+};
 
 
 
