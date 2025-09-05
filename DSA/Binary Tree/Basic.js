7e8266c71e4fba2cfc79a57eef2a826431f3dc58
Ashish Rai <ashishrai8880@gmail.com>
2025-09-05 13:30:37 +0530

    Diameter of binary tree

--- DSA/Binary Tree/Basic.js
+++ DSA/Binary Tree/Basic.js
@@ -355,4 +355,36 @@ var diameterOfBinaryTree = function(root) {
 
 
 
+/**
+Easy Approach of Diameter of binary tree
+
+Time Complexity: O(N*N) where N is the number of nodes in the Binary Tree.
+
+This arises as we calculate the diameter of each node and to calculate the height of its left and right children, we traverse the tree which is proportional to the number of nodes.
+Since this calculation is performed for each node in the tree, the complexity becomes: O(N x N) ~ O(N2).
+Space Complexity : O(1) as no additional data structures or memory is allocated.O(H): Recursive Stack Space is used to calculate the height of the tree at each node which is proportional
+to the height of the tree.The recursive nature of the getHeight function, which incurs space on the call stack for each recursive call until it reaches the leaf nodes or the height of the tree.
+ */
+var diameterOfBinaryTree = function(root) {
+    
+    const util = (ptr)=>{
+        if(ptr == null) return 0 ;
+
+        const left =  util(ptr.left);
+        const right =  util(ptr.right);
+
+        const diameter = left + right ;
+        ans = Math.max(ans , diameter);
+
+        return 1 + Math.max(left , right);
+    }
+
+    ans = 0 ;
+    util(root);
+    return ans ;
+
+};
+
+
+
 
