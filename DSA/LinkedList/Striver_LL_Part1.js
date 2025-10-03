6e4a7826f1e661ac91dc8dd2037d7d5c29db9977
Ashish Rai <ashishrai8880@gmail.com>
2025-10-03 12:50:01 +0530

    Update Striver_LL_Part1.js

--- DSA/LinkedList/Striver_LL_Part1.js
+++ DSA/LinkedList/Striver_LL_Part1.js
@@ -18,3 +18,68 @@ var deleteNode = function(node) {
     node.val = node.next.val ;
     node.next = node.next.next ;
 };
+
+// ===================================================== 2. Count Node in linked list ============================================================================
+getCount(head) {
+        // Code here
+        let count = 0 ;
+        let ptr = head ;
+        while(ptr != null){
+            ptr = ptr.next ;
+            count +=1 ;
+        }
+        return count ;
+    }
+
+// ================================================= 3. Search element ===========================================================================
+searchKey(head, key) {
+        // Code here
+        let ptr = head ;
+        while(ptr != null){
+            if(ptr.data == key){
+                return true ;
+            }
+            ptr = ptr.next ;
+        }
+        return false ;
+    }
+
+
+// ==============================================4. Delete All element equal to given value ====================================================================
+// Leetcode : https://leetcode.com/problems/remove-linked-list-elements/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    while (head !== null && head.val === val) {
+        head = head.next; 
+    }
+
+    let current = head; 
+
+    while (current !== null && current.next !== null) {
+        if (current.next.val === val) {
+            current.next = current.next.next; 
+        } else {
+            current = current.next; 
+        }
+    }
+
+    return head;
+};
+
+
+
+
+
+
+
