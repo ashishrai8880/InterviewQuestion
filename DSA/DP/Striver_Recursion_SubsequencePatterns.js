4cf3ddc770ad2c878f056879c16a6d151ca500ff
Ashish Rai <ashishrai8880@gmail.com>
2025-10-08 18:32:54 +0530

    Update Striver_Recursion_SubsequencePatterns.js

--- DSA/DP/Striver_Recursion_SubsequencePatterns.js
+++ DSA/DP/Striver_Recursion_SubsequencePatterns.js
@@ -969,6 +969,27 @@ class Solution {
 /**
 Leetcode : https://leetcode.com/problems/word-break/ 
 Time Complexity : O (N^3) . 
+
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+Example 2:
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+
+Example 3:
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+ 
  */
 var wordBreak = function(s, wordDict) {
    
@@ -1125,6 +1146,9 @@ var solveSudoku = function(board) {
                         if(  isSafe( i , j , `${n}` )){
 
                             let nextRow = i ; 
+
+
+                            ff
                             let nextCol = j+1 ;
                             if(nextCol==9){   // if control is at last column of row , then move to next row 
                                 nextRow = i + 1 ;
