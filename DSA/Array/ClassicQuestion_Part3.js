ecffba196d03eb46949d6b2049bd9e5200097613
Ashish Rai <ashishrai8880@gmail.com>
2025-10-13 16:44:09 +0530

    Update ClassicQuestion_Part3.js

--- DSA/Array/ClassicQuestion_Part3.js
+++ DSA/Array/ClassicQuestion_Part3.js
@@ -378,9 +378,40 @@ var fourSum = function(nums, target) {
 
 
 
+// ===================================================== 5. Longest Subarray with sum equal to 0 ==============================================================
+// GFG : https://www.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1
+/*
+Given an array arr[] containing both positive and negative integers, the task is to find the length of the longest subarray with a sum equals to 0.
+Note: A subarray is a contiguous part of an array, formed by selecting one or more consecutive elements while maintaining their original order.
+
+Examples:
+Input: arr[] = [15, -2, 2, -8, 1, 7, 10, 23]
+Output: 5
+Explanation: The longest subarray with sum equals to 0 is [-2, 2, -8, 1, 7].
+*/
+
+function maxLen(A, n) {
+  let mpp = new Map();
+  let maxi = 0;
+  let sum = 0;
+
+  for (let i = 0; i < n; i++) {
+    sum += A[i];
+    if (sum === 0) {
+      maxi = i + 1;
+    } else {
+      if (mpp.has(sum)) {
+        maxi = Math.max(maxi, i - mpp.get(sum));
+      } else {
+        mpp.set(sum, i);
+      }
+    }
+  }
+  return maxi;
+}
 
 
-
+// =============================================================
 
 
 
