9306a0ef28e02805197821216ba227a94b126fc4
Ashish Rai <ashishrai8880@gmail.com>
2025-10-21 14:25:24 +0530

    minimum distance between gas station

--- DSA/Binary Search Arrays/Striver_Part2.js
+++ DSA/Binary Search Arrays/Striver_Part2.js
@@ -844,6 +844,63 @@ class Solution {
 
 
 
+// ======================================= 9. Minimise maximum distance between gas station ===============================
+/*
+GFG : https://www.geeksforgeeks.org/problems/minimize-max-distance-to-gas-station/1
+
+We have a horizontal number line. On that number line, we have gas stations at positions stations[0], stations[1], ...,
+stations[n-1]. Now, we add k more gas stations so that d, the maximum distance between adjacent gas stations, is minimized.
+We have to find the smallest possible value of d. Find the answer exactly to 2 decimal places.
+Note: stations is in a strictly increasing order.
+
+Examples:
+Input: stations[] = [1, 2, 3, 4, 5], k = 2
+Output: 1.00
+Explanation: Since all gaps are already equal (1 unit each), adding extra stations in between does not reduce the 
+maximum distance.
+
+Input: stations[] = [3, 6, 12, 19, 33], k = 3
+Output: 6.00 
+Explanation: The largest gap is 14 (between 19 and 33). Adding 2 stations there splits it into approx 4.67. 
+The next largest gap is 7 (between 12 and 19). Adding 1 station splits it into 3.5. Now the maximum gap left is 6.
+*/
+class Solution {
+   
+    minMaxDist(arr, k) {
+        // code here
+        
+        let n = arr.length ;
+        let howManyPlaced = Array.from({length : n-1} , ()=>0);
+        
+        for(let j = 1 ; j<= k ; j++){
+            let maxLen = -1 ;
+            let maxInd = -1 ;
+            for(let i=0 ; i<n-1 ; i++){
+                const diff = arr[i+1] - arr[i];
+                const sectionLength = diff / (howManyPlaced[i]+1) ;
+                
+                if(sectionLength > maxLen){
+                    maxLen = sectionLength ;
+                    maxInd = i ;
+                }
+            }
+            
+            howManyPlaced[maxInd]++ ;
+        }
+        
+        let ans = -Infinity ;
+        
+        for(let i =0 ; i<n-1 ; i++){
+            const diff = arr[i+1] - arr[i];
+            const sectionLength = (diff / (howManyPlaced[i]+1)).toFixed(2) ;
+            ans = Math.max(ans , sectionLength ) ;
+        }
+        
+        return ans == -Infinity ? 0.00 : ans  ;
+        
+    }
+}
+
 
 
  
