cc1895e21f7dcc9d454e4e30b9f4873103aabbc2
Ashish Rai <ashishrai8880@gmail.com>
2025-11-23 12:14:19 +0530

    fruits in basket

--- DSA/Sliding Window and Two Pointer/Striver_Part1.js
+++ DSA/Sliding Window and Two Pointer/Striver_Part1.js
@@ -49,3 +49,216 @@ var lengthOfLongestSubstring = function(s) {
     }
     return res ;
 };
+
+
+// ============================= 2. Maximum Consecutive 1's by flipping at most k 0's ======================================
+
+/**
+Leetcode : https://leetcode.com/problems/max-consecutive-ones-iii/
+Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+Example 1:
+Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+Output: 6
+Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+
+ Brute Force : Break this question into longest subarray with at most k 0's . 
+ */
+var longestOnes = function(nums, k) {
+    
+    let res = 0 ;
+    const n = nums.length ;
+
+    for(let i= 0 ; i<n ; i++){
+        let zCount = 0 ;
+
+        for(let j = i ; j< n ; j++){
+            if(nums[j] == 0 && zCount < k){
+                res = Math.max(res , j-i+1);
+                zCount++ ;
+            }
+            else if(nums[j] == 1){
+                const len = j-i+1 ;
+                res = Math.max(res , len);
+            }
+            else{
+                break ;
+            }
+        }
+    }
+    return res ;
+};
+
+
+// Optimized Way
+/**
+Use 2 Pointer l and r . when zCount increase k then update left l pointer . 
+ */
+var longestOnes = function(nums, k) {
+    
+    let res = 0 ;
+    let r = 0 ;
+    let  l = 0 ;
+    const n = nums.length ;
+    let zCount = 0 ;
+
+    for( ; r<n ; r++){
+        if(nums[r] == 0){
+            zCount++ ;
+        }
+        if(zCount > k){
+            while(zCount > k){
+                if(nums[l] == 0){
+                    zCount-- ;
+                }
+                l++ ;
+            }
+        }
+        res = Math.max(res , (r-l+1));
+    }
+    return res ;
+};
+
+
+
+/**
+Best Optimized : Remove inner loop also . If zero count increase by k , just increase left pointer , so that length will 
+not increase . 
+ */
+var longestOnes = function(nums, k) {
+    
+    let res = 0 ;
+    let r = 0 ;
+    let  l = 0 ;
+    const n = nums.length ;
+    let zCount = 0 ;
+
+    for( ; r<n ; r++){
+        if(nums[r] == 0){
+            zCount++ ;
+        }
+
+        if(zCount > k){
+            if(nums[l] == 0){
+                zCount -- ;
+            }
+            l++ ;
+        }
+
+        res = Math.max(res , (r-l+1));
+    }
+    return res ;
+};
+
+
+
+// ============================================== 3. Fuites Into Baskets =================================================
+/**
+Leetcode : https://leetcode.com/problems/fruit-into-baskets/
+
+You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+
+You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+
+- You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
+- Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+- Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+Given the integer array fruits, return the maximum number of fruits you can pick.
+
+
+Example 1:
+
+Input: fruits = [1,2,1]
+Output: 3
+Explanation: We can pick from all 3 trees.
+Example 2:
+
+Input: fruits = [0,1,2,2]
+Output: 3
+Explanation: We can pick from trees [1,2,2].
+If we had started at the first tree, we would only pick from trees [0,1].
+Example 3:
+
+Input: fruits = [1,2,3,2,2]
+Output: 4
+Explanation: We can pick from trees [2,3,2,2].
+If we had started at the first tree, we would only pick from trees [1,2].
+
+Brute Force Approach : Calculate maximum length of subarray with at most 2 distinct characters or fruits . 
+ * @param {number[]} fruits
+ * @return {number}
+ */
+var totalFruit = function(arr) {
+    
+    let res = 0 ;
+    const n = arr.length ;
+
+    for(let i = 0 ; i < n ; i++){
+        let set = new Set();
+
+        for(let j=i ; j<n ; j++){
+            set.add(arr[j]);
+            if(set.size > 2){
+                break ;
+            }
+            res = Math.max(res , (j-i+1));
+        }
+    }
+    return res ;
+
+};
+
+
+/**
+Better Solution 
+ */
+var totalFruit = function(arr) {
+    
+    let res = 0 ;
+    const n = arr.length ;
+    let map = new Map();
+    let l = 0 ;
+    let r = 0 ;
+
+    for( ; r < n ; r++){
+        map.set(arr[r] , (map.get(arr[r]) || 0) + 1 )
+
+        while(map.size > 2){
+            map.set(arr[l] , map.get(arr[l])-1 );
+            if(map.get(arr[l]) == 0){
+                map.delete(arr[l]);
+            }
+            l++ ;
+        }
+
+        res = Math.max(res , (r-l+1));       
+
+    }
+    return res ;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
