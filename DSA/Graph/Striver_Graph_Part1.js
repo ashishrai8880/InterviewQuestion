b1ab4c8632321a093ab7d0cae5e2597301fd1f2e
Ashish Rai <ashishrai8880@gmail.com>
2026-03-14 14:43:40 +0530

    few question added

--- DSA/Graph/Striver_Graph_Part1.js
+++ DSA/Graph/Striver_Graph_Part1.js
@@ -247,7 +247,7 @@ var floodFill = function(image, sr, sc, color) {
 
 
 // ============================================= 6. Cycle Detection in Undirected Graph ===========================================
-/**
+/** 
 Very Easy Striver Way . Just maintain parent of node . It can be done in bfs and dfs way both . Just need to check 
 if next neighbour is visited and it is not parent of current node , it means someone elese already visit that node
 and created cycle . 
@@ -326,9 +326,174 @@ class Solution {
     }
 }
 
+// ======================================= 7. Cycle Detection in Directed Graph =======================================
+/*
+Now It is not same as previous one . There is difference . Previous Undirected Graph Cycle Detection Cannot apply here . Example : 
 
+      1  -- >  2 --> 3 --> 4
+               ^     |       \
+               |      |        \>5
+         9 <-- 8       -- > 7 --> 5 --> 6
+         \      ^
+          |     |
+          \__> 10
 
+ In above example when u go to 3 -> 4 -> 5 -> 6 -> when return back and start second neighbour of 3 -> 7 -> 5 !! 
+ Now it will say 5 is visited and make it true , but it is not making cycle in that loop . So Above logic won't work . 
 
+ Leetcode : https://leetcode.com/problems/course-schedule/description/
+ GFG : https://www.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1
+
+ Below solution works for Course Schedule 1 also . No Issue .
+*/
+class Solution {
+    isCyclic(n , pre) {
+        
+        let adj = Array.from({length : n} , ()=>[]);
+        
+        pre.forEach((e)=>{
+            const sc = e[1] ;
+            const dst = e[0] ;
+            adj[sc].push(dst) ;
+        })
+        
+        const dfs = (i)=>{
+            
+            vis[i] = true ;
+            path[i] = true ;
+            
+            const v = adj[i] ;
+            
+            for(const n of v){
+                
+                if(vis[n] == false){
+                    vis[n] = true ;
+                    const r = dfs(n) ;
+                    if(r == true) return true ;
+                }
+                
+                else if(vis[n] == true && path[n] == true){
+                    return true ;
+                }
+            }
+            
+            path[i] = false ;
+            
+            return false ;
+        }
+        
+        let vis = Array.from({length : n+1} , ()=>false)
+        let path = Array.from({length : n+1} , ()=>false)
+        
+        let ans = true ;
+        for(let i = 0 ; i<n ; i++){
+            if(vis[i] == false){
+                ans = dfs(i)
+            }
+            if(ans == true) return true ;
+        }
+        return false ;
+    }
+}
+
+// ========================================= 8. Distance of nearest cell having 1 =======================================
+/**
+GFG : https://www.geeksforgeeks.org/problems/distance-of-nearest-cell-having-1-1587115620/1
+Leetcode : https://leetcode.com/problems/01-matrix/
+Similar Question Must Visit : https://leetcode.com/problems/map-of-highest-peak/submissions/1947746293/
+
+Ek Binary Matrix di hai , isme har ek cell ki nearest '1' ki position batani hai . 
+CASE 1 : Agar cell me pehle se '1' hai to nearest distance 0 hogi . 
+CASE 2 : Agar cell me 0 hai , or bilkul bagal wale me 1 hai , iska matlab distance 1 hoga nearest . 
+CASE 3 : Agar cell me 0 hai , or neighbour k neighbour me 1 hai , iska matlab 2 step lena padega to distance 2 hoga . 
+Basically sidhe sidhe 4 direction horizontaly and verticaly move kar sakte hai . 
+
+The distance between two cells (i1, j1)  and (i2, j2) is calculated as |i1 - i2| + |j1 - j2|. 
+You need to return a matrix of the same size, where each cell (i, j) contains the minimum distance from grid[i][j] 
+to the nearest cell having value 1.
+
+Input: grid[][] = [[0, 1, 1, 0], 
+                [1, 1, 0, 0], 
+                [0, 0, 1, 1]]
+Output: [[1, 0, 0, 1], 
+        [0, 0, 1, 1], 
+        [1, 1, 0, 0]]
+
+LOGIC : 0 se nearest 1 tak ka distance search karne ki jagah , BFS use krne ka , or pehle hi store kr lo
+1 kaha kaha hai , or '1' cell k 4 direction me traverse krlo , or step update krdo unki jagah . 
+
+ */
+
+class Solution {
+    nearest(grid) {
+        // code here
+        
+        const row = grid.length ;
+        const col = grid[0].length ;
+        
+        const isValid = (i , j)=>{
+            if(i>=0 && i<row && j>=0 && j<col) return true ;
+            return false ;
+        }
+        
+        let res = Array.from({length : row} , ()=>{
+            return Array.from({length : col} , ()=>0)
+        })
+        
+        let q = [] ;
+        
+        
+        for(let i = 0 ; i<row ; i++){
+            for(let j = 0 ; j<col ; j++){
+                if(grid[i][j] == 1){
+                    q.push([i , j])
+                }
+            }
+        }
+        
+        // console.log({q})
+        let front = 0 ;
+        
+        // while(q.length != 0){
+        while( front < q.length ){
+            
+            const curr = q[front] ;
+            front+=1 ;
+            const [i , j]  = curr ;
+            
+            // up 
+            if(isValid(i-1 , j) && grid[i-1][j] == 0 ){
+                res[i-1][j] = res[i][j] + 1 ;
+                grid[i-1][j] = 1
+                q.push([i-1 , j])
+            }
+            
+            // down
+            if(isValid(i+1 , j)  && grid[i+1][j] == 0 ){
+                res[i+1][j] = res[i][j] + 1 ;
+                grid[i+1][j] = 1
+                q.push([i+1 , j])
+            }
+            
+            // left
+            if(isValid(i , j-1)  && grid[i][j-1] == 0 ){
+                res[i][j-1] = res[i][j] + 1 ;
+                grid[i][j-1] = 1
+                q.push([i , j-1])
+            }
+            
+            // right
+            if(isValid(i , j+1)  && grid[i][j+1] == 0 ){
+                res[i][j+1] = res[i][j] + 1 ;
+                grid[i][j+1] = 1
+                q.push([i , j+1])
+            }
+            
+        }
+        
+        return res ;
+    }
+}
 
 
 
