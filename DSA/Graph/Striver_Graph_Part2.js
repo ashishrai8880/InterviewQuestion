ec81c17c6745b3788fa0e5898b5d6feb322a946c
Ashish Rai <ashishrai8880@gmail.com>
2026-03-16 18:54:03 +0530

    second change

--- DSA/Graph/Striver_Graph_Part2.js
+++ DSA/Graph/Striver_Graph_Part2.js
@@ -51,6 +51,110 @@ class Solution {
     }
 }
 
+// ============================================ 2. Topo Sort BFS Way ====================================================
+/**
+Kanh's Algorithm . Are basically pehle sabki indegree calculate kar leni hai .  Uske baad jiski indeg zero hogi 
+usko queue me store karna hai . 0 indegree matlab , uski dependency kisipe nahi hai , koi bhi node uski taraf
+nahi aa rha hai . Iska matlab topo sort me wo last element hoga . 
+
+Uske baad queue se ek ek karke vertex nikalna hai , or uske neighbours ki indeg-- karte rahe , agar is waqt kisi or
+ki bhi indeg 0 ho jati hai to usko bhi queue me store karlo . 
+ */
+
+class Solution {
+    topoSort(V, edges) {
+        
+        let graph = Array.from( {length : V} , ()=>[]);
+        edges.forEach((e)=>{
+            const sc = e[0] ;
+            const dst = e[1] ;
+            graph[sc].push(dst) ;
+        })
+        
+        const indeg = Array.from({length : V} , ()=>0) ;
+        
+        for(const v of graph){
+            for(const n of v){
+                indeg[n]++ ;
+            }
+        }
+        
+        let q = [] ;
+        for(let i = 0 ; i<V ; i++){
+            if( indeg[i] == 0){
+                q.push(i)
+            }
+        }
+        
+        let topo = [] ;
+        
+        while(q.length != 0){
+            const curr = q.shift() ;
+            topo.push(curr) ;
+            
+            for(const n of graph[curr]){
+                indeg[n]-- ;
+                if(indeg[n] == 0){
+                    q.push(n) ;
+                }
+            }      
+        }     
+        return topo ;
+    }
+}
+
+// ==============================================3. Course Schedule 2 ===================================================
+/**
+Leetcode : https://leetcode.com/problems/course-schedule-ii/
+In this we have to tell series of course a person should start learning so that he is able to complete all course . 
+Are bas topological sort nikalna hai , bas ek or condition check karna hai sath sath , ki cyclic directed graph to nahi 
+hai . To bas Wo condition check + topo sort dfs way ka logic laga rakha hai . Bas . 
+ */
+var findOrder = function(numCourses, pre) {
+    
+    const n = numCourses ;
+    let graph = Array.from({length : n} , ()=>[]);
+
+    pre.forEach((e)=>{
+        const src = e[1] ;
+        const dest = e[0] ;
+        graph[src].push(dest);
+    })
+
+    const dfs = (i)=>{
+        vis[i] = true ;
+        callStack[i] = true ;
+        const vertex = graph[i] ;
+
+        for(const n of vertex){
+            if(vis[n] == false){
+                vis[n] = true ;
+                callStack[n] = true ;
+                dfs(n) ;
+                callStack[n] = false ;
+            }
+            else if(vis[n] == true && callStack[n] == true){
+                ans = false ;
+            }
+        }
+
+        callStack[i] = false ;
+        stack.push(i);
+    }
+
+    let stack = [] ;
+    let vis = Array.from({length : n }, ()=>false) ;
+    let callStack = Array.from({length : n }, ()=>false) ;
+    let ans = true ;
+
+    for(let i = 0 ; i<n ; i++){
+        if(vis[i] == false){
+            dfs(i);
+        }
+    }    
+
+    return ans == true ? stack.reverse() : [];
+};
 
 
 
